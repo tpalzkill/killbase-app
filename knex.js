700c144e23658f04a8f36bcb91bfe375d5991b61
Thomas Palzkill <tpalzkilll@gmail.com>
2017-12-20 11:01:04 -0600

    lost in the sfjkdhjsfg'

--- knex.js
+++ knex.js
@@ -1,4 +1,4 @@
-var environment = process.env.NODE_ENV || 'development';
+var environment = process.env.NODE_ENV || 'production ';
 var config = require('./knexfile')[environment];
 var knex = require('knex')(config);
 
