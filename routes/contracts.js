d05d05ea09f186147dc933451456ec99699ef6b0
Thomas Palzkill <tpalzkilll@gmail.com>
2017-12-22 17:36:58 -0600

    fixing contracts again

--- routes/contracts.js
+++ routes/contracts.js
@@ -101,7 +101,7 @@ router.get('/', function(req, res, next) {
         .leftJoin('clients', 'people.person_id', 'clients.person_id')
         .leftJoin('contracts', 'clients.client_id', 'contracts.client_id')
         .then(function(clientInfo) {
-          console.log('clientinfo',clientInfo);
+          console.log(fullPeopleArr ,clientInfo);
           res.render('contracts', {
             clients: fullPeopleArr,
             contracts: clientInfo
